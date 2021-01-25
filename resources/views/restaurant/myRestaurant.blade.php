f36920ff5d3e1714e2a2e9e6c7c5f5d010d2ce81
Clement-Wds <clementwalshdeserrant@mail.com>
2021-01-25 20:03:22 +0100

    My Restaurant Page

--- resources/views/restaurant/myRestaurant.blade.php
+++ resources/views/restaurant/myRestaurant.blade.php
@@ -4,7 +4,13 @@
 
 <h1>Mon Restaurant</h1>
 
+<h2>{{ $restaurant->name }}</h2>
+<p>ICI LOGO</p>
+<p>{{ $restaurant->postal_address }}</p>
+<p>{{ $restaurant->mail_address }}</p>
+
 <h3>Mes plats</h3>
 
+<p>LISTE DES PLATS</p>
 
 @endsection
